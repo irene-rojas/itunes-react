1ad15793d53619147721a1e70a16e03890f49e97
Irene Rojas <irene@smallwondermedia.com>
2019-07-11 22:22:30 -0400

    Updates

---
+++ precache-manifest.8cc8abd39096504ff2ce5e28a527dfc4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "cc0fc65328e24ce69d8c",
+    "url": "/itunes-react/static/js/runtime~main.cc0fc653.js"
+  },
+  {
+    "revision": "c6f76b6cdd9674bfea40",
+    "url": "/itunes-react/static/js/main.c6f76b6c.chunk.js"
+  },
+  {
+    "revision": "30ea5c8accb317712040",
+    "url": "/itunes-react/static/js/1.30ea5c8a.chunk.js"
+  },
+  {
+    "revision": "c6f76b6cdd9674bfea40",
+    "url": "/itunes-react/static/css/main.fbb1f9a9.chunk.css"
+  },
+  {
+    "revision": "e03f5073c11792b0206af14fd03608f4",
+    "url": "/itunes-react/index.html"
+  }
+];
